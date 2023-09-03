8517619a4e765cac9297182a63c25be7f163a9e1
XenaSit <sitnikovaxena@gmail.com>
2023-09-03 15:05:28 -0700

    second set of challenges are almost done, there is still a stretch challenge, yes there is.

--- js-practice.js
+++ js-practice.js
@@ -1,5 +1,5 @@
 // 💻 Challenges
-
+console.log("CHALLENG NUMER 1")
 // Write the code that will log the output of 34 added to 71.
 console.log(34 + 71)
 // Write the code that will log the output of 67 subtracted from 123.
@@ -64,40 +64,109 @@ console.log(theQuestion.lastIndexOf(""))
 
 // ================================================================================
 
-
-
 // 💻 Challenges
-// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
-
+console.log("CHALLENG NUMER 2")
 // Write the code that will log true or false for the following:
-
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34/3 > 67/2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+var learnLength = "LEARN"
+var studentLength = "Student"
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 && "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"))
 // Does "LEARN" contain the subset "rn"?
+console.log("LEARN".includes("rn"))
 // Does "LEARN"[0] strictly equal "l"?
+console.log("LEARN"[0] === "l")
 // Modify the code from the previous question to return true.
+console.log("LEARN"[0] === "L")
 // Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly working code.
 
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var shoes = 1000
+if (shoes <= 100) {
+    console.log("You alright, go ahead")
+} else {
+    console.log("oh hell nooo!")
+} 
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry = "no"
+if (hungry === "yes") {
+    console.log("Please go take a break")
+} else {
+    console.log("What are you doing? You got work to do!")
+} 
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLightLight = "sdsd"
+// var trafficLightLight = "red"
+// var trafficLightLight = "green"
+// var trafficLightLight = "yellow"
+if (trafficLightLight === "green") {
+    console.log("GOGOGOGO")
+} else if (trafficLightLight === "red") {
+    console.log("STOP!")
+} else if (trafficLightLight === "yellow") {
+    console.log("slow down or speed up, either way")
+} else {
+    console.log("what is going on?")
+}
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+var imSaidNumber1 = 1
+var imSaidNumber2 = 100
+if (imSaidNumber1 == imSaidNumber2) {
+    console.log("We are the same, what? you can't see?")
+} else if (imSaidNumber1 > imSaidNumber2) {
+    console.log(`${imSaidNumber1} is obviously bigger than ${imSaidNumber2}`)
+} else if (imSaidNumber1 < imSaidNumber2) {
+    console.log(`${imSaidNumber1} is obviously smaller than ${imSaidNumber2}`)
+} else {
+    console.log("what is going on?")
+}
+
+
+var imSaidNumber3 = 1
+var imSaidNumber4 = 100
+if (imSaidNumber3 > imSaidNumber4) {
+    console.log(`${imSaidNumber3} is obviously bigger`)
+} else if (imSaidNumber3 < imSaidNumber4) {
+    console.log(`${imSaidNumber4} is obviously bigger`)
+} else {
+    console.log("Nothing is computing in this world")
+}
+
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+var imSaidNumber5 = 2
+if (imSaidNumber5 % 2 === 0) {
+    console.log(`${imSaidNumber5} is obviously an Even Stieven`)
+} else if (imSaidNumber5 % 2 === 1) {
+    console.log(`${imSaidNumber5} is obviously an Odd Todd`)
+} else {
+    console.log("Nothing is computing in this world")
+}
+
 // 🏔 Stretch Goals
 // Write a conditional statement for the following prompts.
 
 // Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+
 // Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
+
 // Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
 
 // ================================================================================
 
 // 💻 Challenges
+// console.log("CHALLENG NUMER 3")
 // Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
 
 // Consider the variable:
