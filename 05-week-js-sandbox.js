8316681dc612dc55af63e21807f1542e50a1c69d
XenaSit <sitnikovaxena@gmail.com>
2023-12-04 11:05:36 -0500

    commit from yesterday

--- 05-week-js-sandbox.js
+++ 05-week-js-sandbox.js
@@ -123,4 +123,27 @@ console.log(whatCentury("1124"));
 console.log(whatCentury("1111"));
 console.log(whatCentury("2222"));
 console.log(whatCentury("3333"));
+
+console.log("==========================================")
+
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+
+
+function min(arr, toReturn) {
+    console.log(arr, toReturn);
+    let value = Math.min.apply(Math, arr)
+    let index = arr.indexOf(value)
+
+    if (toReturn === value) {
+        return value
+    } else {
+        return  index
+    }
+  }
+
+console.log("the Function", min([1,2,3,4,5]));
+
 console.log("==========================================")
