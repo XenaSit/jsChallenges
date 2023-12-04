6bff6d6c0a1f7067cbdb3fa77f5579a36f94094f
XenaSit <sitnikovaxena@gmail.com>
2023-12-04 11:34:09 -0500

    got rid of the same numbers in the array

--- 05-week-js-sandbox.js
+++ 05-week-js-sandbox.js
@@ -147,3 +147,22 @@ function min(arr, toReturn) {
 console.log("the Function", min([1,2,3,4,5]));
 
 console.log("==========================================")
+// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+function repeats(arr){
+    const filterOutNum = arr.filter((value, i, a) => { return a.indexOf(value) === a.lastIndexOf(value)})
+    const sumItUp = 
+};
+
+
+
+
+console.log(repeats([4,5,7,5,4,8]))
+console.log(repeats([9, 10, 19, 13, 19, 13]))
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]))
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]))
+console.log(repeats([5, 10, 19, 13, 10, 13]))
+
+console.log("==========================================")
