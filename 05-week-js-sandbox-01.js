fb24425e3cb84eb54fa40826c6665f5ea0535e70
XenaSit <sitnikovaxena@gmail.com>
2023-12-26 08:54:30 -0500

    new challenge

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -364,3 +364,31 @@ function highAndLow(numbers){
 console.log(highAndLow("1 2 3 4 5")); //return "5 1"
 console.log(highAndLow("1 2 -3 4 5")); //return "5 -3"
 console.log(highAndLow("1 9 3 4 -5")); //return "9 -5"
+
+console.log("==========================================")
+
+
+// DESCRIPTION:
+// You are given an integer N. Your job is to figure out how many substrings inside of N divide evenly with N.
+// Rules:
+// -If an integer is 0, then it does NOT divide evenly into anything.
+// -Even though N can divide evenly with itself, we do not count it towards the end number. For Example:
+
+// N = 23, the answer will be 0.  
+// -If there are multiple instances of a number, they all get counted. For example:
+// N = 11, the answer will be 2 
+// Input:  A non negative integer.
+// Output: / The number of times you found an integer that was evenly divisible with N.
+
+
+// 8 does not evenly divide with 877692. 877692/8 = 109711 with 4 remainder.
+// 7 does not evenly divide with 877692. 877692/7 = 125384 with 4 remainder.
+// 7 does not evenly divide with 877692. 877692/7 = 125384 with 4 remainder.
+// 6 evenly divides with 877692. 877692/6 = 146282 with 0 remainder.
+// 9 does not evenly divide with 877692. 877692/9 = 97521 with 3 remainder.
+// 2 evenly divides with 877692. 877692/2 = 438846 with 0 remainder.
+// We aren't going to stop there though. We need to check ALL of the substrings inside of 877692.
+// 87 does not evenly divide with 877692. 877692/87 = 10088 with 36 remainder.
+// 77 does not evenly divide with 877692. 877692/77 = 11398 with 46 remainder.
+// 76 does not evenly divide with 877692. 877692/76 = 11548 with 44 remainder.
+// 69 does not evenly divide with 877692. 877692/69 = 12720 with 12 remainder.
