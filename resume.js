f15290a72865c5e03f0ee2061b0ed1d6c9704b32
XenaSit <sitnikovaxena@gmail.com>
2024-01-16 19:18:29 -0500

    finnished resume

--- resume.js
+++ resume.js
@@ -4,7 +4,7 @@
 // Cary, North Caroline   |   SitnikovaXena@gmail.com   |   linkedin.com/in/sxena/    |   https://github.com/XenaSit 
 // SKILLS:
 
-// 
+
 
 // Languages: JavaScript, SQL, Ruby
 // Framework: React, Ruby on Rails
@@ -16,52 +16,54 @@
 // Full stack developer Internship
 
 // 2023
-//     • Collected 200+ hours in Ruby and Rails development.
-//     • Facilitated a transition from Sprocket to Webpacker for improved efficiency.
-//     • Collaborated on team projects, updating databases, and deploying websites seamlessly.
-//     • Demonstrated proficiency in managing version updates and live deployments.
+// Collected 200+ hours in Ruby and Rails development.
+// Facilitated a transition from Sprocket to Webpacker for improved efficiency.
+// Customized a Ruby gem for optimized data handling, resulting in a 15% improvement in table-related operations.
+// Successfully migrated assets, culminating in a 25% decline in the project's asset-related issues.
+// Managed the update process, leading to a 15% decrease in post-update bug reports.
+// Deployed updates for 5 critical gems, addressing security concerns and introducing new features.
 
 // Evealed Inc.
 // Full stack developer Internship
 
 // 2020
-//     • Collaborated on enhancing EV-Trips website functionality, contributing to over 200 hours of development in Node.js, MongoDB, Express, and React for a seamless full-stack experience.
-//     • Implemented Test-Driven Development (TDD) principles, ensuring robust and reliable code in collaboration with the team.
-//     • Actively participated in refining and optimizing the map functionality, contributing to the user-friendly and efficient navigation experience on the EV-Trips platform.
-//     • Collaborated with a team of 5 developers to achieve project milestones for EV-Trips.
+// Collaborated on enhancing EV-Trips website functionality, contributing to over 200 hours of development in Node.js, MongoDB, Express, and React for a seamless full-stack experience.
+// Applied Test-Driven Development (TDD) principles, ensuring robust and reliable code in collaboration with the team.
+// Actively participated in refining and optimizing the map functionality, contributing to the user-friendly and efficient navigation experience on the EV-Trips platform.
+// Collaborated with a team of 5 developers to achieve project milestones for EV-Trips.
 
 
 // LEARN Academy, San Diego, CA
 // Full stack Developer
 
 // 2019 – 2020
-//     • Led 3 diverse projects with a focus on Test-Driven Development (TDD).
-//     • Mentored 10 students in HTML, CSS, and JavaScript projects.
-//     • Collaboratively developed 3 full-stack applications through pair programming within 3 teams and effective debugging.
-//     • Compiled near 500 hours of direct coding experience. At an accelerated rate of high-impact learning have built 10 applications at a professional level. 
-//     • Crated Apps with React framework using Webpack, Postgres Database and AWS infrastructure.
+// Led 3 diverse projects with a focus on Test-Driven Development (TDD).
+// Mentored 10 students in HTML, CSS, and JavaScript projects.
+// Collaboratively developed 3 full-stack applications through pair programming within 3 teams and effective debugging.
+// Compiled near 500 hours of direct coding experience. At an accelerated rate of high-impact learning have built 10 applications at a professional level. 
+// Crated Apps with React framework using Webpack, Postgres Database and AWS infrastructure.
 
 // Rosneftegaz Co., Noviy Urengoi, Russia
 // Project Lead
 
 // 2017 – 2019
-//     • Achieved +$100,000 monthly savings through monthly loss analysis of recorded information.
-//     • Connected 15 distant petroleum operation centers and 32 gas stations to the office database, streamlining workflow.
-//     • Strategically personaly prevented 2 thefts through relocation of 20+ security camera on 12 petrolium sites.
-//     • Innovatively averted product dissolution in oil tanks witin 12 oil exploration stations, implementing new site information recording software.
+// Achieved +$100,000 monthly savings through monthly loss analysis of recorded information.
+// Connected 15 distant petroleum operation centers and 32 gas stations to the office database, streamlining workflow.
+// Strategically personally prevented 2 thefts through relocation of 20+ security camera on 12 petroleum sites.
+// Innovatively averted product dissolution in oil tanks within 12 oil exploration stations, implementing new site information recording software.
 
 // Field Supervisor
 // 2014 – 2017
-//     • Analyzed documentation, leading to promotions and personal advancement.
-//     • Successfully promoted +10 team members, improving information analysis for  more efficient issue resolution.
-//     • Implemented a database software at field facilities, detecting oil tank leakage and resulting in savings, equivalent to 50 tons of petrolium a month.
-//     • Increased transparency by integrating remote sites record repositories with office, resulting in a 25% turnover increase.
+// Analyzed documentation, leading to promotions and personal advancement.
+// Successfully promoted +10 team members, improving information analysis for  more efficient issue resolution.
+// Executed a database software at field facilities, detecting oil tank leakage and resulting in savings, equivalent to 50 tons of petroleum a month.
+// Increased transparency by integrating remote sites record repositories with office, resulting in a 25% turnover increase.
 
 // Head of Sales Department
 // 2011 – 2014
-//     • Drove a 50% increase in revenue through strategic team management and thorough database evaluation across 5 operational locations.
-//     • Fostered a positive work environment within 2 months, enhancing transparency and problem-solving between 3 departments.
-//     • Recruited and on-boarded 5-10 team members, conducting exhaustive data assessment.
+// Drove a 50% increase in revenue through strategic team management and thorough database evaluation across 5 operational locations.
+// Fostered a positive work environment within 2 months, enhancing transparency and problem-solving between 3 departments.
+// Recruited and on-boarded 5-10 team members, conducting exhaustive data assessment.
 
 // EDUCATION:
 // Learn Academy
