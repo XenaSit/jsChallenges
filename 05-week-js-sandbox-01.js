3bcfc32d4625b726f9cf77af01e5718fd01eebd7
XenaSit <sitnikovaxena@gmail.com>
2024-01-17 10:55:28 -0500

    that was a quick solve

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -770,14 +770,16 @@ function selectSubarray(arr) {
 console.log("==========================================")
 
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
-
-// Example
-// filter_list([1,2,'a','b']) == [1,2]
-// filter_list([1,'a','b',0,15]) == [1,0,15]
-// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+var filter1 = [1,2,'a','b']
+var filter2 = [1,'a','b',0,15]
+var filter3 = [1,2,'aasf','1','123',123]
 
 function filter_list(l) {
-    // Return a new array with the strings filtered out
+    return l.filter(v => typeof v === 'number');
   }
+
+console.log(filter_list(filter1)); // [1,2]
+console.log(filter_list(filter2)); // [1,0,15]
+console.log(filter_list(filter3)); // [1,2,123]
 
 console.log("==========================================")
