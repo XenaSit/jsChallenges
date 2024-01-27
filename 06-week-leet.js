55856194fd5297b1afdabb345dc500fc234e7696
XenaSit <sitnikovaxena@gmail.com>
2024-01-27 10:33:48 -0500

    regular function

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -174,50 +174,48 @@ var createCounter = function(init) {
  * counter.decrement(); // 4
  */
 
-// console.log("==========================================")
+console.log("==========================================")
 
 // 2635. Apply Transform Over Each Element in Array
-// Easy
-// Companies
-// Hint
 // Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
-
 // The returned array should be created such that returnedArray[i] = fn(arr[i], i).
-
 // Please solve it without the built-in Array.map method.
 
- 
-
 // Example 1:
-
+var arr1 = [1,2,3]
 // Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
 // Output: [2,3,4]
 // Explanation:
 // const newArray = map(arr, plusone); // [2,3,4]
 // The function increases each value in the array by one. 
 // Example 2:
-
+var arr2 = [1,2,3]
 // Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
 // Output: [1,3,5]
 // Explanation: The function increases each value by the index it resides in.
 // Example 3:
-
+var arr3 = [10,20,30]
 // Input: arr = [10,20,30], fn = function constant() { return 42; }
 // Output: [42,42,42]
 // Explanation: The function always returns 42.
  
-
 // Constraints:
-
 // 0 <= arr.length <= 1000
 // -109 <= arr[i] <= 109
 // fn returns a number
 
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
+
+// @param {number[]} arr
+//  @param {Function} fn
+// @return {number[]}
+
 var map = function(arr, fn) {
+    let returnedArray = [];
     
 };
+
+console.log("==========================================")
+
+// console.log("==========================================")
+
+// console.log("==========================================")
