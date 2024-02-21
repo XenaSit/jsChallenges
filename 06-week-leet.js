f3ab768b18a83a6ea48fe9e4702fa2a128acf108
XenaSit <sitnikovaxena@gmail.com>
2024-02-21 13:20:59 -0500

    LEET, branch:leet, the DAily CHALLENGE: Bitwise AND of Numbers Range

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -1180,6 +1180,20 @@ console.log("==========================================")
 // Constraints:
 // 0 <= left <= right <= 231 - 1
 
+// @param {number} left
+// @param {number} right
+// @return {number}
+
+var rangeBitwiseAnd = function(left, right) {
+    let shift = 0;
+    while (left < right) {
+        left >>= 1;
+        right >>= 1;
+        shift++;
+    }
+    return left << shift;
+};
+
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
