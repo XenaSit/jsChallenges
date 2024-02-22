b9828f28f4071d1bda892bb45d2c45b334e08943
XenaSit <sitnikovaxena@gmail.com>
2024-02-21 19:42:11 -0500

    CODEWARS, branch:consecutive, begaan

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1294,6 +1294,14 @@ console.log(lowest(s5)); // 8
 console.log(lowest(s6)); // 0
 console.log("==========================================")
 
+// You are given a list of unique integers arr, and two integers a and b.
+// Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value 
+// (True if a and b are consecutive, False otherwise).
+// It is guaranteed that a and b are both present in arr.
+
+function consecutive(arr, a, b) {
+    // Your code here...
+  }
 
 // console.log("==========================================")
 // console.log("==========================================")
