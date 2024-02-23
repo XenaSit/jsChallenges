a884fb01423644be99f445391bd020e6de12566b
XenaSit <sitnikovaxena@gmail.com>
2024-02-23 14:30:41 -0500

    LEET, branch:leet, the 30 days of JS challenge: ITERVAL CACELLAATION

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -1079,14 +1079,14 @@ var cancellable = function(fn, args, t) {
     // args is an array of arguments that we want to pass to fn when we call it.
     // t is a number of milliseconds that we want to wait before calling fn.
 
-    let timer = setTimeout(() => {
+    let interval = setInterval(() => {
     // This line creates a variable named timer and assigns it the result of calling setTimeout.
     // setTimeout is a built-in function that can execute another function after a delay.
     // The first argument of setTimeout is a function that we want to execute later.
     // The second argument of setTimeout is the delay in milliseconds.
     // In this case, we are using an arrow function as the first argument, which is a shorthand way of writing a function.
     // The arrow function has no parameters and no curly braces, which means it will execute one statement and return its value.
-    fn(...args); // This is the statement that the arrow function will execute. It calls fn with the args as parameters.
+        fn(...args); // This is the statement that the arrow function will execute. It calls fn with the args as parameters.
     // The ... operator is called the spread operator, which can expand an array into individual elements.
     // For example, if args is [2, 3], then fn(...args) is equivalent to fn(2, 3).
     }, t);
@@ -1096,7 +1096,7 @@ var cancellable = function(fn, args, t) {
     // This line returns another function from the cancellable function. This function will act as the cancel function.
     // This function has no parameters and no name. It is an anonymous function.
  
-    clearTimeout(timer); // This line calls clearTimeout, which is another built-in function that can cancel a timer created by setTimeout.
+    clearTimeout(interval); // This line calls clearTimeout, which is another built-in function that can cancel a timer created by setTimeout.
     // The argument of clearTimeout is the timer ID that we want to cancel. In this case, we use the timer variable that we created earlier.
   };
 };
@@ -1246,8 +1246,11 @@ var findJudge = function(n, trust) {
     return -1; // No town judge found
 };
 
-// console.log("==========================================")
-// console.log("==========================================")
+console.log("==========================================")
+
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
