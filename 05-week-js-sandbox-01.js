72f44dade2f81e97a12f845abb98c8a2c52e5035
XenaSit <sitnikovaxena@gmail.com>
2024-02-27 12:15:10 -0500

    CODEWARS, branch:fib done

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1371,7 +1371,37 @@ function isAllPossibilities(x) {
 
 
 console.log("==========================================")
-// console.log("==========================================")
+
+// Fast Fibonacci
+// The generic implementation of the fibonacci algorithm is usually something like the following
+
+// function fib(num) {
+//   if (num < 2) return num;
+//   return fib(num - 1) + fib(num - 2);
+// }
+// Now thats all and well and good but that function isn't too efficient. If I wanted to get the 1000th number in the series, I'd have to wait... days? maybe years?
+
+// Your task
+// Write a more efficient fibonacci function that can calculate the 1000th+ number series without breaking a sweat. Read up on tail call optimization for some help.
+
+// Starting values
+// fib(0) = 0;
+// fib(1) = 1;
+
+
+function fib(num) {
+    if (num < 2) return num;
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= num; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
+}
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 
