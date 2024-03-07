759a02c0391a30d8c03f104f337931947fb62d7b
XenaSit <sitnikovaxena@gmail.com>
2024-03-07 10:33:36 -0500

    LEET, branch:leet, the DAILY challenge: 876. Middle of the Linked List

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -1816,31 +1816,32 @@ var hasCycle = function(head) {
 
 console.log("==========================================")
 
-876. Middle of the Linked List
-Easy
-Topics
-Companies
-Given the head of a singly linked list, return the middle node of the linked list.
-
-If there are two middle nodes, return the second middle node.
-
+// 876. Middle of the Linked List
+// Easy
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
  
 
-Example 1:
+// Definition for singly-linked list.
+// function ListNode(val, next) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+// }
 
+// @param {ListNode} head
+// @return {ListNode}
 
-Input: head = [1,2,3,4,5]
-Output: [3,4,5]
-Explanation: The middle node of the list is node 3.
-Example 2:
-
-
-Input: head = [1,2,3,4,5,6]
-Output: [4,5,6]
-Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
- 
-
-Constraints:
+var middleNode = function(head) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
