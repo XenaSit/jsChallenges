8536c6cb30eaa1e6c6133b49df3934765871cdc6
XenaSit <sitnikovaxena@gmail.com>
2024-03-09 09:37:22 -0500

    LEET, branch:leet, the DAILY challenge: 2540. Minimum Common Value

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -1922,14 +1922,18 @@ console.log("==========================================")
 // @return {number}
 
 var getCommon = function(nums1, nums2) {
-    for (let i = 0; i < 0; i++) {
-        if (nums1[i] === nums2[i]) {
-            return nums1[i]
+    let i = 0, j = 0;
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i];
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
         }
-        return nums1[i]
     }
+    return -1; // No common value found
 };
-
 
 console.log("==========================================")
 // console.log("==========================================")
