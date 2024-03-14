f4683ee84079ff20d84813cda974eb7b687b59c0
XenaSit <sitnikovaxena@gmail.com>
2024-03-14 16:11:26 -0400

    LEET, branch:leet, the DAILY challenge: 930. Binary Subarrays With Sum

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2113,6 +2113,31 @@ var pivotInteger = function(n) {
 
 console.log("==========================================")
 
+// 930. Binary Subarrays With Sum
+// Medium
+// Topics
+// Companies
+// Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+// A subarray is a contiguous part of the array.
+// Example 1:
+// Input: nums = [1,0,1,0,1], goal = 2
+// Output: 4
+// Explanation: The 4 subarrays are bolded and underlined below:
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// Example 2:
+// Input: nums = [0,0,0,0,0], goal = 0
+// Output: 15
+
+// @param {number[]} nums
+// @param {number} goal
+// @return {number}
+
+var numSubarraysWithSum = function(nums, goal) {
+    
+};
 
 
 console.log("==========================================")
