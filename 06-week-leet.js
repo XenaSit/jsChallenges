4fcc80f2e9cf2919d2beda8c6952071a430d480e
XenaSit <sitnikovaxena@gmail.com>
2024-03-17 11:10:52 -0400

    LEET, branch:leet, the DAILY challenge: 57. Insert Interval

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2253,7 +2253,31 @@ console.log("==========================================")
 // @return {number[][]}
 
 var insert = function(intervals, newInterval) {
-    
+    const result = [];
+    let i = 0;
+    const n = intervals.length;
+
+    // Add all intervals that end before the new interval starts
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    // Merge intervals that overlap with the new interval
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    result.push(newInterval);
+
+    // Add remaining intervals
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
 };
 
 console.log("==========================================")
