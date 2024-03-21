adc88a5bcc1578bd9c7da7f959baddef83dbed6c
XenaSit <sitnikovaxena@gmail.com>
2024-03-21 12:35:52 -0400

    LEET, branch:leet, the DAILY challenge: 206. Reverse Linked List

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2482,6 +2482,21 @@ Example 3:
 Input: head = []
 Output: []
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+};
+
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
