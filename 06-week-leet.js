f1fdaf38c7927556add2025bee4448e947e87d06
XenaSit <sitnikovaxena@gmail.com>
2024-03-23 09:42:25 -0400

    LEET, branch:leet, the DAILY challenge: 143. Reorder List

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2543,43 +2543,29 @@ var isPalindrome = function(head) {
 
 console.log("==========================================")
 
-143. Reorder List
-Medium
-Topics
-Companies
-You are given the head of a singly linked-list. The list can be represented as:
+// 143. Reorder List
+// Medium
+// You are given the head of a singly linked-list. The list can be represented as:
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+// Example 1:
+// Input: head = [1,2,3,4]
+// Output: [1,4,2,3]
+// Example 2:
+// Input: head = [1,2,3,4,5]
+// Output: [1,5,2,4,3]
 
-L0 → L1 → … → Ln - 1 → Ln
-Reorder the list to be on the following form:
+// Definition for singly-linked list.
+// function ListNode(val, next) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+// }
 
-L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
-You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+// @param {ListNode} head
+// @return {void} Do not return anything, modify head in-place instead.
 
- 
-
-Example 1:
-
-
-Input: head = [1,2,3,4]
-Output: [1,4,2,3]
-Example 2:
-
-
-Input: head = [1,2,3,4,5]
-Output: [1,5,2,4,3]
-
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {void} Do not return anything, modify head in-place instead.
- */
 var reorderList = function(head) {
     
 };
