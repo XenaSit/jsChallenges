63460a931bdc8030159e1ceed78aa239b40d6c7c
XenaSit <sitnikovaxena@gmail.com>
2024-03-25 11:15:51 -0400

    LEET, branch:leet, the DAILY challenge: 442. Find All Duplicates in an Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2642,7 +2642,31 @@ var findDuplicate = function(nums) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+442. Find All Duplicates in an Array
+Medium
+Topics
+Companies
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+Example 3:
+
+Input: nums = [1]
+Output: []
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
