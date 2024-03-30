16bfba6b61e3568311e697acf36d522964a7ff3b
XenaSit <sitnikovaxena@gmail.com>
2024-03-30 10:30:19 -0400

    LEET, branch:leet, the DAILY challenge: 992. Subarrays with K Different Integers

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -2855,35 +2855,25 @@ var countSubarrays = function(nums, k) {
 console.log("==========================================")
 
 
-992. Subarrays with K Different Integers
-Hard
-Topics
-Companies
-Given an integer array nums and an integer k, return the number of good subarrays of nums.
+// 992. Subarrays with K Different Integers
+// Hard
+// Given an integer array nums and an integer k, return the number of good subarrays of nums.
+// A good array is an array where the number of different integers in that array is exactly k.
+// For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+// A subarray is a contiguous part of an array.
+// Example 1:
+// Input: nums = [1,2,1,2,3], k = 2
+// Output: 7
+// Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+// Example 2:
+// Input: nums = [1,2,1,3,4], k = 3
+// Output: 3
+// Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
 
-A good array is an array where the number of different integers in that array is exactly k.
+// @param {number[]} nums
+// @param {number} k
+// @return {number}
 
-For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
-A subarray is a contiguous part of an array.
-
- 
-
-Example 1:
-
-Input: nums = [1,2,1,2,3], k = 2
-Output: 7
-Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
-Example 2:
-
-Input: nums = [1,2,1,3,4], k = 3
-Output: 3
-Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var subarraysWithKDistinct = function(nums, k) {
     
 };
