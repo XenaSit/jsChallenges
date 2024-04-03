caf36768afb9ab6ac912d87ef633d0b706a7744d
XenaSit <sitnikovaxena@gmail.com>
2024-04-03 10:57:37 -0400

    LEET, branch:leet, the DAILY challenge: 79. Word Search

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -3036,37 +3036,24 @@ var isIsomorphic = function(s, t) {
 
 console.log("==========================================")
 
-79. Word Search
-Medium
-Topics
-Companies
-Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+// 79. Word Search
+// Medium
+// Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+// Example 1:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
+// Example 2:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
+// Example 3:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
 
-The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+// @param {character[][]} board
+// @param {string} word
+// @return {boolean}
 
- 
-
-Example 1:
-
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
-Output: true
-Example 2:
-
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
-Output: true
-Example 3:
-
-
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
-Output: false
-
-/**
- * @param {character[][]} board
- * @param {string} word
- * @return {boolean}
- */
 var exist = function(board, word) {
     
 };
