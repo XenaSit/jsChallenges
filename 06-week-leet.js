bf8de219fee5559f6351b9373f4ab9027e75610d
XenaSit <sitnikovaxena@gmail.com>
2024-04-14 08:58:39 -0400

    LEET, branch:leet, the DAILY challenge: 404. Sum of Left Leaves

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -3712,6 +3712,21 @@ Example 2:
 Input: root = [1]
 Output: 0
  
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
