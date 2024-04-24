15c8f50eedac3a9f6adaa212d928ad8c219243ea
XenaSit <sitnikovaxena@gmail.com>
2024-04-24 10:49:35 -0400

    LEET, branch:leet, the DAILY challenge: 1137. N-th Tribonacci Number

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -4397,7 +4397,12 @@ var tribonacci = function(n) {
     if (n === 0) return 0;
     if (n === 1 || n === 2) return 1;
     
-    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    let dp = [0, 1, 1];
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    
+    return dp[n];
 };
 
 console.log("==========================================")
