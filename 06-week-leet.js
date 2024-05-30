d0dbb00f12ecc0d46a0871190b67c2695656b3ac
XenaSit <sitnikovaxena@gmail.com>
2024-05-30 09:10:13 -0400

    LEET, branch:leet, the DAILY challenge: 1442. Count Triplets That Can Form Two Arrays of Equal XOR

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6564,7 +6564,20 @@ console.log("==========================================")
 // @return {number}
 
 var countTriplets = function(arr) {
-    
+    let count = 0;
+    const n = arr.length;
+
+    for (let i = 0; i < n; ++i) {
+        let xor = 0;
+        for (let k = i; k < n; ++k) {
+            xor ^= arr[k];
+            if (xor === 0 && k > i) {
+                count += (k - i);
+            }
+        }
+    }
+
+    return count;
 };
 
 console.log("==========================================")
