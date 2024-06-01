9bf769f39eacdf8fa32ca49f990b418bfcc07c72
XenaSit <sitnikovaxena@gmail.com>
2024-06-01 16:37:18 -0400

    LEET, branch:leet, the DAILY challenge: 3110. Score of a String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6650,7 +6650,14 @@ console.log("==========================================")
 // @return {number}
 
 var scoreOfString = function(s) {
+    let score = 0;
     
+    for (let i = 1; i < s.length; i++) {
+        let diff = Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+        score += diff;
+    }
+    
+    return score;
 };
 
 console.log("==========================================")
