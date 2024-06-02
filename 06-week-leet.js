e0e5872eaea0d626cbc95021963f12d8c30186b4
XenaSit <sitnikovaxena@gmail.com>
2024-06-02 09:44:37 -0400

    LEET, branch:leet, the DAILY challenge: 344. Reverse String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6679,7 +6679,19 @@ console.log("==========================================")
 // @return {void} Do not return anything, modify s in-place instead.
 
 var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
     
+    while (left < right) {
+        // Swap the elements at left and right
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        
+        // Move the pointers towards each other
+        left++;
+        right--;
+    }
 };
 
 console.log("==========================================")
