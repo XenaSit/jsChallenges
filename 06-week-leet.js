809785e3285b9c4a8f5e10f32cdcbb851376088b
XenaSit <sitnikovaxena@gmail.com>
2024-06-02 09:42:57 -0400

    LEET, branch:leet, the DAILY challenge: 344. Reverse String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6662,33 +6662,26 @@ var scoreOfString = function(s) {
 
 console.log("==========================================")
 
-344. Reverse String
-Easy
-Topics
-Companies
-Hint
-Write a function that reverses a string. The input string is given as an array of characters s.
+// 344. Reverse String
+// Easy
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
 
-You must do this by modifying the input array in-place with O(1) extra memory.
+// Example 1:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
 
- 
+// Example 2:
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
 
-Example 1:
+// @param {character[]} s
+// @return {void} Do not return anything, modify s in-place instead.
 
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-Example 2:
-
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
-
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
 var reverseString = function(s) {
     
 };
+
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
