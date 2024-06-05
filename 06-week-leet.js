d15d05f1b1925f91717a4d87b6cb6d9979be0f7c
XenaSit <sitnikovaxena@gmail.com>
2024-06-05 11:12:08 -0400

    LEET, branch:leet, the DAILY challenge: 1002. Find Common Characters

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6791,7 +6791,25 @@ var longestPalindrome = function(s) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+1002. Find Common Characters
+Easy
+Topics
+Companies
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
