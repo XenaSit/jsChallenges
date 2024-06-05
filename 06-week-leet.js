5f9ecefc3089f519d7ff58f3e2794e7f368a68f8
XenaSit <sitnikovaxena@gmail.com>
2024-06-05 11:13:56 -0400

    LEET, branch:leet, the DAILY challenge: 1002. Find Common Characters

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6792,27 +6792,22 @@ var longestPalindrome = function(s) {
 
 console.log("==========================================")
 
-1002. Find Common Characters
-Easy
-Topics
-Companies
-Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+// 1002. Find Common Characters
+// Easy
+// Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). 
+// You may return the answer in any order.
 
- 
+// Example 1:
+// Input: words = ["bella","label","roller"]
+// Output: ["e","l","l"]
 
-Example 1:
+// Example 2:
+// Input: words = ["cool","lock","cook"]
+// Output: ["c","o"]
 
-Input: words = ["bella","label","roller"]
-Output: ["e","l","l"]
-Example 2:
+// @param {string[]} words
+// @return {string[]}
 
-Input: words = ["cool","lock","cook"]
-Output: ["c","o"]
-
-/**
- * @param {string[]} words
- * @return {string[]}
- */
 var commonChars = function(words) {
     
 };
