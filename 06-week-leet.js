c27d30fc200a8cc1b9fb1d1e92e8a26ec3cbe2eb
XenaSit <sitnikovaxena@gmail.com>
2024-06-06 10:49:31 -0400

    LEET, branch:leet, the DAILY challenge: 846. Hand of Straights

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -6842,34 +6842,29 @@ var commonChars = function(words) {
 
 console.log("==========================================")
 
-846. Hand of Straights
-Medium
-Topics
-Companies
-Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, 
-and consists of groupSize consecutive cards.
+// 846. Hand of Straights
+// Medium
+// Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, 
+// and consists of groupSize consecutive cards.
 
-Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, 
-return true if she can rearrange the cards, or false otherwise.
+// Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, 
+// return true if she can rearrange the cards, or false otherwise.
 
- 
+// Example 1:
 
-Example 1:
+// Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+// Output: true
+// Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
 
-Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
-Output: true
-Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
-Example 2:
+// Example 2:
+// Input: hand = [1,2,3,4,5], groupSize = 4
+// Output: false
+// Explanation: Alice's hand can not be rearranged into groups of 4.
 
-Input: hand = [1,2,3,4,5], groupSize = 4
-Output: false
-Explanation: Alice's hand can not be rearranged into groups of 4.
+// @param {number[]} hand
+// @param {number} groupSize
+// @return {boolean}
 
-/**
- * @param {number[]} hand
- * @param {number} groupSize
- * @return {boolean}
- */
 var isNStraightHand = function(hand, groupSize) {
     
 };
