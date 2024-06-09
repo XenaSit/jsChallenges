452b3a90d6660c11f857b880c318148d84acb78b
XenaSit <sitnikovaxena@gmail.com>
2024-06-09 08:37:10 -0400

    LEET, branch:leet, the DAILY challenge: 974. Subarray Sums Divisible by K

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -7002,30 +7002,25 @@ var checkSubarraySum = function(nums, k) {
 
 console.log("==========================================")
 
-974. Subarray Sums Divisible by K
-Medium
-Topics
-Companies
-Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+// 974. Subarray Sums Divisible by K
+// Medium
+// Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+// A subarray is a contiguous part of an array.
 
-A subarray is a contiguous part of an array.
+// Example 1:
+// Input: nums = [4,5,0,-2,-3,1], k = 5
+// Output: 7
+// Explanation: There are 7 subarrays with a sum divisible by k = 5:
+// [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
 
-Example 1:
+// Example 2:
+// Input: nums = [5], k = 9
+// Output: 0
 
-Input: nums = [4,5,0,-2,-3,1], k = 5
-Output: 7
-Explanation: There are 7 subarrays with a sum divisible by k = 5:
-[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
-Example 2:
+// @param {number[]} nums
+// @param {number} k
+// @return {number}
 
-Input: nums = [5], k = 9
-Output: 0
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var subarraysDivByK = function(nums, k) {
     
 };
