0d0294cbe1c8ab59f83ffb7b827d62072e28f428
XenaSit <sitnikovaxena@gmail.com>
2024-06-12 08:16:01 -0400

    LEET, branch:leet, the DAILY challenge: 75. Sort Colors

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -7167,7 +7167,22 @@ console.log("==========================================")
 // @return {void} Do not return anything, modify nums in-place instead.
 
 var sortColors = function(nums) {
+    let low = 0, mid = 0, high = nums.length - 1;
     
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            // Swap nums[low] and nums[mid]
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else if (nums[mid] === 2) {
+            // Swap nums[mid] and nums[high]
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
 };
 
 console.log("==========================================")
