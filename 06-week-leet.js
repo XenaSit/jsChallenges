c42b1afe9c4592418d8bcfb3e1e3fb7f346e4616
XenaSit <sitnikovaxena@gmail.com>
2024-06-14 10:40:50 -0400

    LEET, branch:leet, the DAILY challenge: 945. Minimum Increment to Make Array Unique

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -7253,34 +7253,26 @@ var minMovesToSeat = function(seats, students) {
 
 console.log("==========================================")
 
-945. Minimum Increment to Make Array Unique
-Medium
-Topics
-Companies
-You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1.
+// 945. Minimum Increment to Make Array Unique
+// Medium
+// You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1.
+// Return the minimum number of moves to make every value in nums unique.
+// The test cases are generated so that the answer fits in a 32-bit integer.
 
-Return the minimum number of moves to make every value in nums unique.
+// Example 1:
+// Input: nums = [1,2,2]
+// Output: 1
+// Explanation: After 1 move, the array could be [1, 2, 3].
 
-The test cases are generated so that the answer fits in a 32-bit integer.
+// Example 2:
+// Input: nums = [3,2,1,2,1,7]
+// Output: 6
+// Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+// It can be shown with 5 or less moves that it is impossible for the array to have all unique values.
 
- 
+// @param {number[]} nums
+// @return {number}
 
-Example 1:
-
-Input: nums = [1,2,2]
-Output: 1
-Explanation: After 1 move, the array could be [1, 2, 3].
-Example 2:
-
-Input: nums = [3,2,1,2,1,7]
-Output: 6
-Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
-It can be shown with 5 or less moves that it is impossible for the array to have all unique values.
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var minIncrementForUnique = function(nums) {
     
 };
