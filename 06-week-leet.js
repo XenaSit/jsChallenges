8ae2d3a5665e531f92a398c21b778b139e82c4f7
XenaSit <sitnikovaxena@gmail.com>
2024-06-22 09:29:53 -0400

    LEET, branch:leet, the DAILY challenge: 1248. Count Number of Nice Subarrays

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -7787,37 +7787,29 @@ var maxSatisfied = function(customers, grumpy, minutes) {
 
 console.log("==========================================")
 
-1248. Count Number of Nice Subarrays
-Medium
-Topics
-Companies
-Hint
-Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+// 1248. Count Number of Nice Subarrays
+// Medium
+// Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+// Return the number of nice sub-arrays.
 
-Return the number of nice sub-arrays.
+// Example 1:
+// Input: nums = [1,1,2,1,1], k = 3
+// Output: 2
+// Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
 
- 
+// Example 2:
+// Input: nums = [2,4,6], k = 1
+// Output: 0
+// Explanation: There are no odd numbers in the array.
 
-Example 1:
+// Example 3:
+// Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+// Output: 16
 
-Input: nums = [1,1,2,1,1], k = 3
-Output: 2
-Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
-Example 2:
+// @param {number[]} nums
+// @param {number} k
+// @return {number}
 
-Input: nums = [2,4,6], k = 1
-Output: 0
-Explanation: There are no odd numbers in the array.
-Example 3:
-
-Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
-Output: 16
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var numberOfSubarrays = function(nums, k) {
     
 };
