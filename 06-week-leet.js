3dfac5d9c7d8b55eed7df71be6ca8ecec2d77862
XenaSit <sitnikovaxena@gmail.com>
2024-06-25 08:16:24 -0400

    LEET, branch:leet, the DAILY challenge: 1038. Binary Search Tree to Greater Sum Tree

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -7993,6 +7993,22 @@ Example 2:
 Input: root = [0,null,1]
 Output: [1,null,1]
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    
+};
+
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
