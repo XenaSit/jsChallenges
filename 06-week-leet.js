3a0ef278ce57a1bbb766fd7b913d99304f01d896
XenaSit <sitnikovaxena@gmail.com>
2024-07-01 08:04:23 -0400

    LEET, branch:leet, the DAILY challenge: 1550. Three Consecutive Odds

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -8385,29 +8385,23 @@ var maxNumEdgesToRemove = function(n, edges) {
 
 console.log("==========================================")
 
-1550. Three Consecutive Odds
-Easy
-Topics
-Companies
-Hint
-Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+// 1550. Three Consecutive Odds
+// Easy
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
  
+// Example 1:
+// Input: arr = [2,6,4,1]
+// Output: false
+// Explanation: There are no three consecutive odds.
 
-Example 1:
+// Example 2:
+// Input: arr = [1,2,34,3,4,5,7,23,12]
+// Output: true
+// Explanation: [5,7,23] are three consecutive odds.
 
-Input: arr = [2,6,4,1]
-Output: false
-Explanation: There are no three consecutive odds.
-Example 2:
+// @param {number[]} arr
+// @return {boolean}
 
-Input: arr = [1,2,34,3,4,5,7,23,12]
-Output: true
-Explanation: [5,7,23] are three consecutive odds.
-
-/**
- * @param {number[]} arr
- * @return {boolean}
- */
 var threeConsecutiveOdds = function(arr) {
     
 };
