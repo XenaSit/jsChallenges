8a606c8c4ffaf68fce055a5bb791e420a4294480
XenaSit <sitnikovaxena@gmail.com>
2024-07-04 12:26:32 -0400

    LEET, branch:leet, the DAILY challenge: 2181. Merge Nodes in Between Zeros

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -8513,52 +8513,40 @@ var minDifference = function(nums) {
 
 console.log("==========================================")
 
-2181. Merge Nodes in Between Zeros
-Medium
-Topics
-Companies
-Hint
-You are given the head of a linked list, which contains a series of integers separated by 0's. 
-The beginning and end of the linked list will have Node.val == 0.
+// 2181. Merge Nodes in Between Zeros
+// Medium
+// You are given the head of a linked list, which contains a series of integers separated by 0's. 
+// The beginning and end of the linked list will have Node.val == 0.
+// For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. 
+// The modified list should not contain any 0's.
+// Return the head of the modified linked list.
 
-For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. 
-The modified list should not contain any 0's.
+// Example 1:
+// Input: head = [0,3,1,0,4,5,2,0]
+// Output: [4,11]
+// Explanation: 
+// The above figure represents the given linked list. The modified list contains
+// - The sum of the nodes marked in green: 3 + 1 = 4.
+// - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
 
-Return the head of the modified linked list.
+// Example 2:
+// Input: head = [0,1,0,3,0,2,2,0]
+// Output: [1,3,4]
+// Explanation: 
+// The above figure represents the given linked list. The modified list contains
+// - The sum of the nodes marked in green: 1 = 1.
+// - The sum of the nodes marked in red: 3 = 3.
+// - The sum of the nodes marked in yellow: 2 + 2 = 4.
 
- 
+// Definition for singly-linked list.
+// function ListNode(val, next) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+// }
 
-Example 1:
+// @param {ListNode} head
+// @return {ListNode}
 
-
-Input: head = [0,3,1,0,4,5,2,0]
-Output: [4,11]
-Explanation: 
-The above figure represents the given linked list. The modified list contains
-- The sum of the nodes marked in green: 3 + 1 = 4.
-- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
-Example 2:
-
-
-Input: head = [0,1,0,3,0,2,2,0]
-Output: [1,3,4]
-Explanation: 
-The above figure represents the given linked list. The modified list contains
-- The sum of the nodes marked in green: 1 = 1.
-- The sum of the nodes marked in red: 3 = 3.
-- The sum of the nodes marked in yellow: 2 + 2 = 4.
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
 var mergeNodes = function(head) {
     
 };
