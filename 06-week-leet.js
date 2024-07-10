06fe744082721dbbc062bf671e947fddc04efd22
XenaSit <sitnikovaxena@gmail.com>
2024-07-10 09:55:04 -0400

    LEET, branch:leet, the DAILY challenge: 1598. Crawler Log Folder

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -8854,48 +8854,33 @@ var averageWaitingTime = function(customers) {
 
 console.log("==========================================")
 
-1598. Crawler Log Folder
-Easy
-Topics
-Companies
-Hint
-The Leetcode file system keeps a log each time some user performs a change folder operation.
+// 1598. Crawler Log Folder
+// Easy
+// The Leetcode file system keeps a log each time some user performs a change folder operation.
+// The operations are described below:
+// "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+// "./" : Remain in the same folder.
+// "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+// You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+// The file system starts in the main folder, then the operations in logs are performed.
+// Return the minimum number of operations needed to go back to the main folder after the change folder operations.
 
-The operations are described below:
+// Example 1:
+// Input: logs = ["d1/","d2/","../","d21/","./"]
+// Output: 2
+// Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
 
-"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
-"./" : Remain in the same folder.
-"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
-You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+// Example 2:
+// Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+// Output: 3
+// Example 3:
 
-The file system starts in the main folder, then the operations in logs are performed.
+// Input: logs = ["d1/","../","../","../"]
+// Output: 0
 
-Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+// @param {string[]} logs
+// @return {number}
 
- 
-
-Example 1:
-
-
-
-Input: logs = ["d1/","d2/","../","d21/","./"]
-Output: 2
-Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
-Example 2:
-
-
-
-Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
-Output: 3
-Example 3:
-
-Input: logs = ["d1/","../","../","../"]
-Output: 0
-
-/**
- * @param {string[]} logs
- * @return {number}
- */
 var minOperations = function(logs) {
     
 };
