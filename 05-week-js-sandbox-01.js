d7fea41df9c3dffc6e60c3a3d49a92cfd0b8df6b
XenaSit <sitnikovaxena@gmail.com>
2024-07-15 11:19:38 -0400

    LEET, branch:leet, the DAILY challenge: 2196. Create Binary Tree From Descriptions

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1782,7 +1782,21 @@ Output: [1,2,null,null,3,4]
 Explanation: The root node is the node with value 1 since it has no parent.
 The resulting binary tree is shown in the diagram.
 
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[][]} descriptions
+ * @return {TreeNode}
+ */
+var createBinaryTree = function(descriptions) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
