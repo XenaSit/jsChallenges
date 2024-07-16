4dec33c00139d7fd78a59a28b9678ac72b4c7c1a
XenaSit <sitnikovaxena@gmail.com>
2024-07-16 10:05:50 -0400

    LEET, branch:leet, the DAILY challenge: 2096. Step-By-Step Directions From a Binary Tree Node to Another

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1840,49 +1840,42 @@ var createBinaryTree = function(descriptions) {
 
 console.log("==========================================")
 
-2096. Step-By-Step Directions From a Binary Tree Node to Another
-Medium
-Topics
-Companies
-Hint
-You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+// 2096. Step-By-Step Directions From a Binary Tree Node to Another
+// Medium
+// You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. 
+// You are also given an integer startValue representing the value of the start node s, 
+// and a different integer destValue representing the value of the destination node t.
+// Find the shortest path starting from node s and ending at node t. 
+// Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. 
+// Each letter indicates a specific direction:
 
-Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+// 'L' means to go from a node to its left child node.
+// 'R' means to go from a node to its right child node.
+// 'U' means to go from a node to its parent node.
+// Return the step-by-step directions of the shortest path from node s to node t.
 
-'L' means to go from a node to its left child node.
-'R' means to go from a node to its right child node.
-'U' means to go from a node to its parent node.
-Return the step-by-step directions of the shortest path from node s to node t.
+// Example 1:
+// Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+// Output: "UURL"
+// Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
 
- 
+// Example 2:
+// Input: root = [2,1], startValue = 2, destValue = 1
+// Output: "L"
+// Explanation: The shortest path is: 2 → 1.
 
-Example 1:
+// Definition for a binary tree node.
+// function TreeNode(val, left, right) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+// }
 
+// @param {TreeNode} root
+// @param {number} startValue
+// @param {number} destValue
+// @return {string}
 
-Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
-Output: "UURL"
-Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
-Example 2:
-
-
-Input: root = [2,1], startValue = 2, destValue = 1
-Output: "L"
-Explanation: The shortest path is: 2 → 1.
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} startValue
- * @param {number} destValue
- * @return {string}
- */
 var getDirections = function(root, startValue, destValue) {
     
 };
