b3cc3678feeb8d49ecedc361f881826284d6a4a0
XenaSit <sitnikovaxena@gmail.com>
2024-07-16 10:03:07 -0400

    LEET, branch:leet, the DAILY challenge: 2096. Step-By-Step Directions From a Binary Tree Node to Another

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1839,7 +1839,38 @@ var createBinaryTree = function(descriptions) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+2096. Step-By-Step Directions From a Binary Tree Node to Another
+Medium
+Topics
+Companies
+Hint
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+'L' means to go from a node to its left child node.
+'R' means to go from a node to its right child node.
+'U' means to go from a node to its parent node.
+Return the step-by-step directions of the shortest path from node s to node t.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+Output: "UURL"
+Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
+Example 2:
+
+
+Input: root = [2,1], startValue = 2, destValue = 1
+Output: "L"
+Explanation: The shortest path is: 2 → 1.
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
