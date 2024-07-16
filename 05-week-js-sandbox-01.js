9d16010177033c3314b80cfe54360a928d3035ab
XenaSit <sitnikovaxena@gmail.com>
2024-07-16 10:03:51 -0400

    LEET, branch:leet, the DAILY challenge: 2096. Step-By-Step Directions From a Binary Tree Node to Another

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1869,6 +1869,23 @@ Input: root = [2,1], startValue = 2, destValue = 1
 Output: "L"
 Explanation: The shortest path is: 2 → 1.
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+var getDirections = function(root, startValue, destValue) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
