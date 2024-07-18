1462da1a9cd42a47deeff5ad32d91db92a730820
XenaSit <sitnikovaxena@gmail.com>
2024-07-18 11:28:45 -0400

    LEET, branch:leet, the DAILY challenge: 1530. Number of Good Leaf Nodes Pairs

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -1994,7 +1994,22 @@ Input: root = [7,1,4,6,null,5,3,null,null,null,null,null,2], distance = 3
 Output: 1
 Explanation: The only good pair is [2,5].
 
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} distance
+ * @return {number}
+ */
+var countPairs = function(root, distance) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
