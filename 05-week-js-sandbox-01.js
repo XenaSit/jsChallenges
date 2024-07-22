6b1d0e67b677b1940d92be660d4bbd34e8ab3e59
XenaSit <sitnikovaxena@gmail.com>
2024-07-22 10:18:51 -0400

    LEET, branch:leet, the DAILY challenge: 2418. Sort the People

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -2241,35 +2241,26 @@ var buildMatrix = function(k, rowConditions, colConditions) {
 
 console.log("==========================================")
 
-2418. Sort the People
-Easy
-Topics
-Companies
-Hint
-You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+// 2418. Sort the People
+// Easy
+// You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
+// Return names sorted in descending order by the people's heights.
 
-For each index i, names[i] and heights[i] denote the name and height of the ith person.
+// Example 1:
+// Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+// Output: ["Mary","Emma","John"]
+// Explanation: Mary is the tallest, followed by Emma and John.
 
-Return names sorted in descending order by the people's heights.
+// Example 2:
+// Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+// Output: ["Bob","Alice","Bob"]
+// Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
 
- 
+// @param {string[]} names
+// @param {number[]} heights
+// @return {string[]}
 
-Example 1:
-
-Input: names = ["Mary","John","Emma"], heights = [180,165,170]
-Output: ["Mary","Emma","John"]
-Explanation: Mary is the tallest, followed by Emma and John.
-Example 2:
-
-Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
-Output: ["Bob","Alice","Bob"]
-Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
-
-/**
- * @param {string[]} names
- * @param {number[]} heights
- * @return {string[]}
- */
 var sortPeople = function(names, heights) {
     
 };
