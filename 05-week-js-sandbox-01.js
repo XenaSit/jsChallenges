9b9f7c51f2fa003131d904fe51d3932dea79245a
XenaSit <sitnikovaxena@gmail.com>
2024-07-25 10:09:38 -0400

    LEET, branch:leet, the DAILY challenge: 912. Sort an Array

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -2380,27 +2380,26 @@ var sortJumbled = function(mapping, nums) {
 
 console.log("==========================================")
 
-912. Sort an Array
-Medium
-Given an array of integers nums, sort the array in ascending order and return it.
+// 912. Sort an Array
+// Medium
+// Given an array of integers nums, sort the array in ascending order and return it.
 
-You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 
-Example 1:
-Input: nums = [5,2,3,1]
-Output: [1,2,3,5]
-Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), 
-while the positions of other numbers are changed (for example, 1 and 5).
+// Example 1:
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), 
+// while the positions of other numbers are changed (for example, 1 and 5).
 
-Example 2:
-Input: nums = [5,1,1,2,0,0]
-Output: [0,0,1,1,2,5]
-Explanation: Note that the values of nums are not necessairly unique.
+// Example 2:
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+// Explanation: Note that the values of nums are not necessairly unique.
 
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
+// @param {number[]} nums
+// @return {number[]}
+
 var sortArray = function(nums) {
     
 };
