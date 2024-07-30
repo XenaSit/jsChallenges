ab2fcdd8eb523412e041ba90b87e6705660f3d27
XenaSit <sitnikovaxena@gmail.com>
2024-07-30 09:56:39 -0400

    LEET, branch:leet, the DAILY challenge: 1653. Minimum Deletions to Make String Balanced

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -2850,7 +2850,18 @@ console.log("==========================================")
 // @return {number}
 
 var minimumDeletions = function(s) {
-    
+    let countB = 0;
+    let deletions = 0;
+
+    for (let char of s) {
+        if (char === 'a') {
+            deletions = Math.min(deletions + 1, countB);
+        } else {
+            countB++;
+        }
+    }
+
+    return deletions;
 };
 
 console.log("==========================================")
