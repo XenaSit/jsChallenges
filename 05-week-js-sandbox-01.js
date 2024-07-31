005029eadd74e2624fd716590699bfc6473e6785
XenaSit <sitnikovaxena@gmail.com>
2024-07-31 16:00:01 -0400

    LEET, branch:leet, the DAILY challenge: 1105. Filling Bookcase Shelves

--- 05-week-js-sandbox-01.js
+++ 05-week-js-sandbox-01.js
@@ -2899,7 +2899,23 @@ console.log("==========================================")
 // @return {number}
 
 var minHeightShelves = function(books, shelfWidth) {
-    
+    let n = books.length;
+    let dp = new Array(n + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let i = 1; i <= n; i++) {
+        let width = 0;
+        let height = 0;
+
+        for (let j = i - 1; j >= 0; j--) {
+            width += books[j][0];
+            if (width > shelfWidth) break;
+            height = Math.max(height, books[j][1]);
+            dp[i] = Math.min(dp[i], dp[j] + height);
+        }
+    }
+
+    return dp[n];
 };
 
 console.log("==========================================")
