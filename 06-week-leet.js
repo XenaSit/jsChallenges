a25cb391e9493b41ac7a2929f89b0feba64170b3
XenaSit <sitnikovaxena@gmail.com>
2024-08-19 06:33:32 -0400

    LEET, branch:leet, the DAILY challenge: 650. 2 Keys Keyboard

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -9411,7 +9411,14 @@ console.log("==========================================")
 // @return {number}
 
 var minSteps = function(n) {
-    
+    let steps = 0;
+    for (let i = 2; i <= n; i++) {
+        while (n % i === 0) {
+            steps += i;
+            n /= i;
+        }
+    }
+    return steps;
 };
 
 
