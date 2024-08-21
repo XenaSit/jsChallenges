dd8c039d8d8b0bfa801aa1ef7e497e7f3075eec7
XenaSit <sitnikovaxena@gmail.com>
2024-08-21 06:23:19 -0400

    LEET, branch:leet, the DAILY challenge: 664. Strange Printer

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -9480,33 +9480,26 @@ var stoneGameII = function(piles) {
 
 console.log("==========================================")
 
-664. Strange Printer
-Hard
-Topics
-Companies
-There is a strange printer with the following two special properties:
+// 664. Strange Printer
+// Hard
+// There is a strange printer with the following two special properties:
+// The printer can only print a sequence of the same character each time.
+// At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
+// Given a string s, return the minimum number of turns the printer needed to print it.
 
-The printer can only print a sequence of the same character each time.
-At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
-Given a string s, return the minimum number of turns the printer needed to print it.
+// Example 1:
+// Input: s = "aaabbb"
+// Output: 2
+// Explanation: Print "aaa" first and then print "bbb".
 
- 
+// Example 2:
+// Input: s = "aba"
+// Output: 2
+// Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
 
-Example 1:
+// @param {string} s
+// @return {number}
 
-Input: s = "aaabbb"
-Output: 2
-Explanation: Print "aaa" first and then print "bbb".
-Example 2:
-
-Input: s = "aba"
-Output: 2
-Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
-
-/**
- * @param {string} s
- * @return {number}
- */
 var strangePrinter = function(s) {
     
 };
