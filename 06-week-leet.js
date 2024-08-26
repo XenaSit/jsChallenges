078604b2e900b50852c42d5ec350f0b7b45269c0
XenaSit <sitnikovaxena@gmail.com>
2024-08-26 06:39:06 -0400

    LEET, branch:leet, the DAILY challenge: 590. N-ary Tree Postorder Traversal

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -9733,7 +9733,21 @@ Example 2:
 Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
 
+/**
+ * // Definition for a _Node.
+ * function _Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
 
+/**
+ * @param {_Node|null} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+    
+};
 
 
 console.log("==========================================")
