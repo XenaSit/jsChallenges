c5601eacf7ae36ad8ecafa87aa549b95030a428f
XenaSit <sitnikovaxena@gmail.com>
2024-09-07 14:29:27 -0400

    LEET, branch:leet, the DAILY challenge: 1367. Linked List in Binary Tree

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -10428,7 +10428,29 @@ Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null
 Output: false
 Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
 
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSubPath = function(head, root) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
