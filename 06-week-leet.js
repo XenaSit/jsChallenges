588e66dcfca045526f1e7f01de20f93102a570cb
XenaSit <sitnikovaxena@gmail.com>
2024-09-12 10:37:28 -0400

    LEET, branch:leet, the DAILY challenge: 1684. Count the Number of Consistent Strings

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -10733,38 +10733,31 @@ var minBitFlips = function(start, goal) {
 
 console.log("==========================================")
 
-1684. Count the Number of Consistent Strings
-Easy
-Topics
-Companies
-Hint
-You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+// 1684. Count the Number of Consistent Strings
+// Easy
+// You are given a string allowed consisting of distinct characters and an array of strings words. 
+// A string is consistent if all characters in the string appear in the string allowed.
+// Return the number of consistent strings in the array words.
 
-Return the number of consistent strings in the array words.
+// Example 1:
+// Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+// Output: 2
+// Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
 
- 
+// Example 2:
+// Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+// Output: 7
+// Explanation: All strings are consistent.
 
-Example 1:
+// Example 3:
+// Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+// Output: 4
+// Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
 
-Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
-Output: 2
-Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
-Example 2:
+// @param {string} allowed
+// @param {string[]} words
+// @return {number}
 
-Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
-Output: 7
-Explanation: All strings are consistent.
-Example 3:
-
-Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
-Output: 4
-Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
-
-/**
- * @param {string} allowed
- * @param {string[]} words
- * @return {number}
- */
 var countConsistentStrings = function(allowed, words) {
     
 };
