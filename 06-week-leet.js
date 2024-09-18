b4c7dc0e4d3644a378dfd4d0e9fe7bdf3520e664
Bedel Looshon <sitnikovaxena@gmail.com>
2024-09-18 06:44:30 -0400

    LEET, branch:leet, the Daily challenge: 179. Largest Number

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -11023,29 +11023,22 @@ var uncommonFromSentences = function(s1, s2) {
 
 console.log("==========================================")
 
-179. Largest Number
-Medium
-Topics
-Companies
-Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+// 179. Largest Number
+// Medium
+// Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+// Since the result may be very large, so you need to return a string instead of an integer.
 
-Since the result may be very large, so you need to return a string instead of an integer.
+// Example 1:
+// Input: nums = [10,2]
+// Output: "210"
 
- 
+// Example 2:
+// Input: nums = [3,30,34,5,9]
+// Output: "9534330"
 
-Example 1:
+// @param {number[]} nums
+// @return {string}
 
-Input: nums = [10,2]
-Output: "210"
-Example 2:
-
-Input: nums = [3,30,34,5,9]
-Output: "9534330"
-
-/**
- * @param {number[]} nums
- * @return {string}
- */
 var largestNumber = function(nums) {
     
 };
