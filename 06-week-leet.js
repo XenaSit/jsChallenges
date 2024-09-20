fecd464b928f81656ce57bfc4d42ad71b7e14390
Bedel Looshon <sitnikovaxena@gmail.com>
2024-09-20 06:45:25 -0400

    LEET, branch:leet, the Daily challenge: 214. Shortest Palindrome

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -11122,7 +11122,29 @@ var diffWaysToCompute = function(expression) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+214. Shortest Palindrome
+Hard
+Topics
+Companies
+You are given a string s. You can convert s to a 
+palindrome
+ by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+ 
+
+Example 1:
+
+Input: s = "aacecaaa"
+Output: "aaacecaaa"
+Example 2:
+
+Input: s = "abcd"
+Output: "dcbabcd"
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
