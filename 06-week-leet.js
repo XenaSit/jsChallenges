e2428f88013899f477e446f2dc59f22cbb0c3582
Bedel Looshon <sitnikovaxena@gmail.com>
2024-09-22 07:41:04 -0400

    LEET, branch:leet, the Daily challenge: 440. K-th Smallest in Lexicographical Order

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -11212,7 +11212,22 @@ var lexicalOrder = function(n) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+440. K-th Smallest in Lexicographical Order
+Hard
+Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+
+Example 1:
+Input: n = 13, k = 2
+Output: 10
+Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+
+Example 2:
+Input: n = 1, k = 1
+Output: 1
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
