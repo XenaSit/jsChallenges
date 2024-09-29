7cbe0fbd2dbb11dc10d69e6c68ea7b35cd453d2a
XenaSit <sitnikovaxena@gmail.com>
2024-09-29 07:06:27 -0400

    LEET, branch:leet, the DAILY challenge: 432. All Oone Data Structure

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -11713,75 +11713,70 @@ MyCircularDeque.prototype.isFull = function() {
 
 console.log("==========================================")
 
-432. All Oone Data Structure
-Hard
-Design a data structure to store the strings' count with the ability to return the strings with minimum and maximum counts.
-Implement the AllOne class:
-AllOne() Initializes the object of the data structure.
-inc(String key) Increments the count of the string key by 1. If key does not exist in the data structure, insert it with count 1.
-dec(String key) Decrements the count of the string key by 1. If the count of key is 0 after the decrement, 
-remove it from the data structure. It is guaranteed that key exists in the data structure before the decrement.
-getMaxKey() Returns one of the keys with the maximal count. If no element exists, return an empty string "".
-getMinKey() Returns one of the keys with the minimum count. If no element exists, return an empty string "".
-Note that each function must run in O(1) average time complexity.
+// 432. All Oone Data Structure
+// Hard
+// Design a data structure to store the strings' count with the ability to return the strings with minimum and maximum counts.
+// Implement the AllOne class:
+// AllOne() Initializes the object of the data structure.
+// inc(String key) Increments the count of the string key by 1. If key does not exist in the data structure, insert it with count 1.
+// dec(String key) Decrements the count of the string key by 1. If the count of key is 0 after the decrement, 
+// remove it from the data structure. It is guaranteed that key exists in the data structure before the decrement.
+// getMaxKey() Returns one of the keys with the maximal count. If no element exists, return an empty string "".
+// getMinKey() Returns one of the keys with the minimum count. If no element exists, return an empty string "".
+// Note that each function must run in O(1) average time complexity.
 
-Example 1:
-Input: ["AllOne", "inc", "inc", "getMaxKey", "getMinKey", "inc", "getMaxKey", "getMinKey"]
-[[], ["hello"], ["hello"], [], [], ["leet"], [], []]
-Output: [null, null, null, "hello", "hello", null, "hello", "leet"]
+// Example 1:
+// Input: ["AllOne", "inc", "inc", "getMaxKey", "getMinKey", "inc", "getMaxKey", "getMinKey"]
+// [[], ["hello"], ["hello"], [], [], ["leet"], [], []]
+// Output: [null, null, null, "hello", "hello", null, "hello", "leet"]
 
-Explanation
-AllOne allOne = new AllOne();
-allOne.inc("hello");
-allOne.inc("hello");
-allOne.getMaxKey(); // return "hello"
-allOne.getMinKey(); // return "hello"
-allOne.inc("leet");
-allOne.getMaxKey(); // return "hello"
-allOne.getMinKey(); // return "leet"
+// Explanation
+// AllOne allOne = new AllOne();
+// allOne.inc("hello");
+// allOne.inc("hello");
+// allOne.getMaxKey(); // return "hello"
+// allOne.getMinKey(); // return "hello"
+// allOne.inc("leet");
+// allOne.getMaxKey(); // return "hello"
+// allOne.getMinKey(); // return "leet"
 
 var AllOne = function() {
     
 };
 
-/** 
- * @param {string} key
- * @return {void}
- */
+// @param {string} key
+// @return {void}
+
 AllOne.prototype.inc = function(key) {
     
 };
 
-/** 
- * @param {string} key
- * @return {void}
- */
+// @param {string} key
+// @return {void}
+
 AllOne.prototype.dec = function(key) {
     
 };
 
-/**
- * @return {string}
- */
+// @return {string}
+
 AllOne.prototype.getMaxKey = function() {
     
 };
 
-/**
- * @return {string}
- */
+// @return {string}
+
 AllOne.prototype.getMinKey = function() {
     
 };
 
-/** 
- * Your AllOne object will be instantiated and called as such:
- * var obj = new AllOne()
- * obj.inc(key)
- * obj.dec(key)
- * var param_3 = obj.getMaxKey()
- * var param_4 = obj.getMinKey()
- */
+// Your AllOne object will be instantiated and called as such:
+// var obj = new AllOne()
+// obj.inc(key)
+// obj.dec(key)
+// var param_3 = obj.getMaxKey()
+// var param_4 = obj.getMinKey()
+
 
 console.log("==========================================")
 // console.log("==========================================")
