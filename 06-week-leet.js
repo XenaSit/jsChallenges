39ff39d1a3942101e2da8e907f0e6587a03293f9
XenaSit <sitnikovaxena@gmail.com>
2024-10-05 06:35:24 -0400

    LEET, branch:leet, the DAILY challenge: 567. Permutation in String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -12181,8 +12181,33 @@ var dividePlayers = function(skill) {
     
     return chemistrySum;  // Return the sum of chemistry
 };
-// console.log("==========================================")
-// console.log("==========================================")
+console.log("==========================================")
+
+567. Permutation in String
+Medium
+Topics
+Companies
+Hint
+Given two strings s1 and s2, return true if s2 contains a 
+permutation
+ of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+ 
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
