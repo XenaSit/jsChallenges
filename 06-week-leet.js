dc4469d8602a0ed4687a179ea613588165f9e1f2
XenaSit <sitnikovaxena@gmail.com>
2024-10-05 06:36:57 -0400

    LEET, branch:leet, the DAILY challenge: 567. Permutation in String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -12183,34 +12183,24 @@ var dividePlayers = function(skill) {
 };
 console.log("==========================================")
 
-567. Permutation in String
-Medium
-Topics
-Companies
-Hint
-Given two strings s1 and s2, return true if s2 contains a 
-permutation
- of s1, or false otherwise.
+// 567. Permutation in String
+// Medium
+// Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+// In other words, return true if one of s1's permutations is the substring of s2.
 
-In other words, return true if one of s1's permutations is the substring of s2.
+// Example 1:
+// Input: s1 = "ab", s2 = "eidbaooo"
+// Output: true
+// Explanation: s2 contains one permutation of s1 ("ba").
 
+// Example 2:
+// Input: s1 = "ab", s2 = "eidboaoo"
+// Output: false
  
+// @param {string} s1
+// @param {string} s2
+// @return {boolean}
 
-Example 1:
-
-Input: s1 = "ab", s2 = "eidbaooo"
-Output: true
-Explanation: s2 contains one permutation of s1 ("ba").
-Example 2:
-
-Input: s1 = "ab", s2 = "eidboaoo"
-Output: false
- 
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {boolean}
- */
 var checkInclusion = function(s1, s2) {
     
 };
