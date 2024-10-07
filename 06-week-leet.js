b6c2ac2b4e7ca1b56a56bc9240d938b2fa8f4270
XenaSit <sitnikovaxena@gmail.com>
2024-10-07 06:52:03 -0400

    LEET, branch:leet, the DAILY challenge: 2696. Minimum String Length After Removing Substrings

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -12302,33 +12302,32 @@ var areSentencesSimilar = function(sentence1, sentence2) {
 };
 console.log("==========================================")
 
-2696. Minimum String Length After Removing Substrings
-Easy
-You are given a string s consisting only of uppercase English letters.
-You can apply some operations to this string where, in one operation, 
-you can remove any occurrence of one of the substrings "AB" or "CD" from s.
-Return the minimum possible length of the resulting string that you can obtain.
-Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+// 2696. Minimum String Length After Removing Substrings
+// Easy
+// You are given a string s consisting only of uppercase English letters.
+// You can apply some operations to this string where, in one operation, 
+// you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+// Return the minimum possible length of the resulting string that you can obtain.
+// Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
 
-Example 1:
-Input: s = "ABFCACDB"
-Output: 2
-Explanation: We can do the following operations:
-- Remove the substring "ABFCACDB", so s = "FCACDB".
-- Remove the substring "FCACDB", so s = "FCAB".
-- Remove the substring "FCAB", so s = "FC".
-So the resulting length of the string is 2.
-It can be shown that it is the minimum length that we can obtain.
+// Example 1:
+// Input: s = "ABFCACDB"
+// Output: 2
+// Explanation: We can do the following operations:
+// - Remove the substring "ABFCACDB", so s = "FCACDB".
+// - Remove the substring "FCACDB", so s = "FCAB".
+// - Remove the substring "FCAB", so s = "FC".
+// So the resulting length of the string is 2.
+// It can be shown that it is the minimum length that we can obtain.
 
-Example 2:
-Input: s = "ACBBD"
-Output: 5
-Explanation: We cannot do any operations on the string so the length remains the same.
+// Example 2:
+// Input: s = "ACBBD"
+// Output: 5
+// Explanation: We cannot do any operations on the string so the length remains the same.
 
-/**
- * @param {string} s
- * @return {number}
- */
+// @param {string} s
+// @return {number}
+
 var minLength = function(s) {
     
 };
