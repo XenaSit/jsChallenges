045e735207f4263c1cf1c38b3a10963d50f277c8
XenaSit <sitnikovaxena@gmail.com>
2024-10-17 06:27:31 -0400

    LEET, branch:leet, the DAILY challenge: 670. Maximum Swap

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -12885,8 +12885,33 @@ var longestDiverseString = function(a, b, c) {
     // Return the result array as a string
     return result.join('');
 };
+
+
+
+
 console.log("==========================================")
-// console.log("==========================================")
+
+670. Maximum Swap
+Medium
+You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+
+Return the maximum valued number you can get.
+
+ 
+
+Example 1:
+
+Input: num = 2736
+Output: 7236
+Explanation: Swap the number 2 and the number 7.
+Example 2:
+
+Input: num = 9973
+Output: 9973
+Explanation: No swap.
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
