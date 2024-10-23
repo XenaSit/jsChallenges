47133c373de304c4b04eea933ecaa514631959b6
XenaSit <sitnikovaxena@gmail.com>
2024-10-23 06:49:44 -0400

    LEET, branch:leet, the DAILY challenge: 2641. Cousins in Binary Tree II

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -13276,14 +13276,9 @@ Topics
 Companies
 Hint
 Given the root of a binary tree, replace the value of each node in the tree with the sum of all its cousins' values.
-
 Two nodes of a binary tree are cousins if they have the same depth with different parents.
-
 Return the root of the modified tree.
-
 Note that the depth of a node is the number of edges in the path from the root node to it.
-
- 
 
 Example 1:
 
@@ -13307,6 +13302,21 @@ Explanation: The diagram above shows the initial binary tree and the binary tree
 - Node with value 1 does not have any cousins so its sum is 0.
 - Node with value 2 does not have any cousins so its sum is 0.
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var replaceValueInTree = function(root) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
