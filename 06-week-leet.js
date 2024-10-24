83fe4c73afef2f6a2f94c74c4cf7ce9741fe738e
XenaSit <sitnikovaxena@gmail.com>
2024-10-24 07:02:31 -0400

    LEET, branch:leet, the DAILY challenge: 951. Flip Equivalent Binary Trees

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -13347,39 +13347,37 @@ var replaceValueInTree = function(root) {
 
 console.log("==========================================")
 
-951. Flip Equivalent Binary Trees
-Medium
-For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
-A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
-Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
+// 951. Flip Equivalent Binary Trees
+// Medium
+// For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+// A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+// Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
 
-Example 1:
-Flipped Trees Diagram
-Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
-Output: true
-Explanation: We flipped at nodes with values 1, 3, and 5.
+// Example 1:
+// Flipped Trees Diagram
+// Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+// Output: true
+// Explanation: We flipped at nodes with values 1, 3, and 5.
 
-Example 2:
-Input: root1 = [], root2 = []
-Output: true
+// Example 2:
+// Input: root1 = [], root2 = []
+// Output: true
 
-Example 3:
-Input: root1 = [], root2 = [1]
-Output: false
+// Example 3:
+// Input: root1 = [], root2 = [1]
+// Output: false
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root1
- * @param {TreeNode} root2
- * @return {boolean}
- */
+// Definition for a binary tree node.
+// function TreeNode(val, left, right) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+//  }
+
+// @param {TreeNode} root1
+// @param {TreeNode} root2
+// @return {boolean}
+
 var flipEquiv = function(root1, root2) {
     
 };
