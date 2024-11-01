d1308321f1a84d87a2b5735fd10e87bd08364462
XenaSit <sitnikovaxena@gmail.com>
2024-11-01 06:39:05 -0400

    LEET, branch:leet, the DAILY challenge: 1957. Delete Characters to Make Fancy String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -13823,7 +13823,43 @@ var minimumTotalDistance = function(robot, factory) {
     return dp[m][n];
 };
 console.log("==========================================")
-// console.log("==========================================")
+
+1957. Delete Characters to Make Fancy String
+Easy
+Topics
+Companies
+Hint
+A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+ 
+
+Example 1:
+
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+Example 2:
+
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+Example 3:
+
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
