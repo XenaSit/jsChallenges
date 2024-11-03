d2680741c45dd3a4a1bfe4fbe654f4b9ca5cd71b
XenaSit <sitnikovaxena@gmail.com>
2024-11-03 06:31:42 -0500

    LEET, branch:leet, the DAILY challenge: 796. Rotate String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -13941,7 +13941,30 @@ var isCircularSentence = function(sentence) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+796. Rotate String
+Easy
+Topics
+Companies
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
