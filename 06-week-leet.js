9667e140386c6d53e143d8ab37d276e95ed0b549
XenaSit <sitnikovaxena@gmail.com>
2024-11-11 06:24:45 -0500

    LEET, branch:leet, the DAILY challenge: 2601. Prime Subtraction Operation

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -14366,19 +14366,12 @@ console.log("==========================================")
 
 2601. Prime Subtraction Operation
 Medium
-Topics
-Companies
-Hint
 You are given a 0-indexed integer array nums of length n.
-
 You can perform the following operation as many times as you want:
-
 Pick an index i that you haven’t picked before, and pick a prime p strictly less than nums[i], then subtract p from nums[i].
 Return true if you can make nums a strictly increasing array using the above operation and false otherwise.
 
 A strictly increasing array is an array whose each element is strictly greater than its preceding element.
-
- 
 
 Example 1:
 
@@ -14398,6 +14391,13 @@ Input: nums = [5,8,3]
 Output: false
 Explanation: It can be proven that there is no way to perform operations to make nums sorted in strictly increasing order, so the answer is false.
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var primeSubOperation = function(nums) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
