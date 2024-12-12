a4754d033f09707da6b2b94009b7d5f11fa84fb9
XenaSit <sitnikovaxena@gmail.com>
2024-12-12 10:10:58 -0500

    LEET, branch:leet, the DAILY challenge: 2558. Take Gifts From the Richest Pile

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -16308,20 +16308,13 @@ console.log("==========================================")
 
 2558. Take Gifts From the Richest Pile
 Easy
-Topics
-Companies
-Hint
 You are given an integer array gifts denoting the number of gifts in various piles. Every second, you do the following:
-
 Choose the pile with the maximum number of gifts.
 If there is more than one pile with the maximum number of gifts, choose any.
 Leave behind the floor of the square root of the number of gifts in the pile. Take the rest of the gifts.
 Return the number of gifts remaining after k seconds.
 
- 
-
 Example 1:
-
 Input: gifts = [25,64,9,4,100], k = 4
 Output: 29
 Explanation: 
@@ -16331,8 +16324,8 @@ The gifts are taken in the following way:
 - After that the first pile is chosen and 5 gifts are left behind.
 - Finally, the last pile is chosen again and 3 gifts are left behind.
 The final remaining gifts are [5,8,9,4,3], so the total number of gifts remaining is 29.
-Example 2:
 
+Example 2:
 Input: gifts = [1,1,1,1], k = 4
 Output: 4
 Explanation: 
@@ -16340,6 +16333,13 @@ In this case, regardless which pile you choose, you have to leave behind 1 gift 
 That is, you can't take any pile with you. 
 So, the total gifts remaining are 4.
 
+@param {number[]} gifts
+@param {number} k
+@return {number}
+
+var pickGifts = function(gifts, k) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
