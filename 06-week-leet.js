7f2fb8246b1a548d99d3149893f97c046b683da7
XenaSit <sitnikovaxena@gmail.com>
2024-12-19 11:50:02 -0500

    LEET, branch:leet, the DAILY challenge: 769. Max Chunks To Make Sorted

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -16884,7 +16884,17 @@ console.log("==========================================")
 // @return {number}
 
 var maxChunksToSorted = function(arr) {
-    
+    let maxSoFar = 0;
+    let chunks = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        maxSoFar = Math.max(maxSoFar, arr[i]);
+        if (maxSoFar === i) {
+            chunks++;
+        }
+    }
+
+    return chunks;
 };
 
 console.log("==========================================")
