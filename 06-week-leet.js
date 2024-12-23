ce0ff76d0911de085597a897b323da55ba30beb2
XenaSit <sitnikovaxena@gmail.com>
2024-12-23 13:04:13 -0500

    LEET, branch:leet, the DAILY challenge: 2471. Minimum Number of Operations to Sort a Binary Tree by Level

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -17176,6 +17176,19 @@ Input: root = [1,2,3,4,5,6]
 Output: 0
 Explanation: Each level is already sorted in increasing order so return 0.
 
+Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+@param {TreeNode} root
+@return {number}
+
+var minimumOperations = function(root) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
