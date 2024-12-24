64b3e0fa6023f5a6fee416b81448800cbd27d57e
XenaSit <sitnikovaxena@gmail.com>
2024-12-24 12:13:02 -0500

    LEET, branch:leet, the DAILY challenge: 3203. Find Minimum Diameter After Merging Two Trees

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -17272,9 +17272,54 @@ console.log("==========================================")
 // @param {number[][]} edges2
 // @return {number}
 
+/**
+ * @param {number[][]} edges1
+ * @param {number[][]} edges2
+ * @return {number}
+ */
 var minimumDiameterAfterMerge = function(edges1, edges2) {
+    // Calculate diameters of both trees
+    const d1 = treeDiameter(edges1);
+    const d2 = treeDiameter(edges2);
     
+    // Return maximum of:
+    // 1. Diameter of first tree
+    // 2. Diameter of second tree
+    // 3. Path through connecting edge (half of d1 + half of d2 + 1)
+    return Math.max(d1, d2, Math.ceil(d1 / 2) + Math.ceil(d2 / 2) + 1);
 };
+function treeDiameter(edges) {
+    // Create adjacency list representation of the tree
+    const n = edges.length + 1;
+    const g = Array.from({ length: n }, () => []);
+    for (const [a, b] of edges) {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    
+    let [ans, a] = [0, 0];  // ans: max distance, a: farthest node
+    const dfs = (i, fa, t) => {
+        // Visit all neighbors except parent
+        for (const j of g[i]) {
+            if (j !== fa) {
+                dfs(j, i, t + 1);
+            }
+        }
+        
+        // Update maximum distance and farthest node
+        if (ans < t) {
+            ans = t;
+            a = i;
+        }
+    };
+    
+    // First DFS to find one end of diameter
+    dfs(0, -1, 0);
+    // Second DFS from found node to get actual diameter
+    dfs(a, -1, 0);
+    
+    return ans;
+}
 
 
 console.log("==========================================")
