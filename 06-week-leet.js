e69600a9c936dfab815159b7bc7c1abb19c4d5f7
XenaSit <sitnikovaxena@gmail.com>
2024-12-24 12:07:55 -0500

    LEET, branch:leet, the DAILY challenge: 3203. Find Minimum Diameter After Merging Two Trees

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -17242,6 +17242,42 @@ function countMinimumSwaps(arr) {
 }
 
 console.log("==========================================")
+
+3203. Find Minimum Diameter After Merging Two Trees
+Hard
+There exist two undirected trees with n and m nodes, numbered from 0 to n - 1 and from 0 to m - 1, respectively. 
+You are given two 2D integer arrays edges1 and edges2 of lengths n - 1 and m - 1, 
+respectively, where edges1[i] = [ai, bi] indicates that there is an edge between nodes ai and bi 
+in the first tree and edges2[i] = [ui, vi] indicates that there is an edge between nodes ui and vi in the second tree.
+
+You must connect one node from the first tree with another node from the second tree with an edge.
+
+Return the minimum possible diameter of the resulting tree.
+
+The diameter of a tree is the length of the longest path between any two nodes in the tree.
+
+Example 1:
+Input: edges1 = [[0,1],[0,2],[0,3]], edges2 = [[0,1]]
+Output: 3
+Explanation:
+We can obtain a tree of diameter 3 by connecting node 0 from the first tree with any node from the second tree.
+
+Example 2:
+Input: edges1 = [[0,1],[0,2],[0,3],[2,4],[2,5],[3,6],[2,7]], edges2 = [[0,1],[0,2],[0,3],[2,4],[2,5],[3,6],[2,7]]
+Output: 5
+Explanation:
+We can obtain a tree of diameter 5 by connecting node 0 from the first tree with node 0 from the second tree.
+
+ 
+
+
+console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
