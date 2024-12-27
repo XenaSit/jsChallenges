5bd0331c12e6289aced2a5e5e5e9d515d26dadda
XenaSit <sitnikovaxena@gmail.com>
2024-12-27 12:24:02 -0500

    LEET, branch:leet, the DAILY challenge: 1014. Best Sightseeing Pair

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -17423,25 +17423,25 @@ var findTargetSumWays = function(nums, target) {
 
 console.log("==========================================")
 
-1014. Best Sightseeing Pair
-Medium
-You are given an integer array values where values[i] represents the value of the ith sightseeing spot. 
-Two sightseeing spots i and j have a distance j - i between them.
-The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: 
-the sum of the values of the sightseeing spots, minus the distance between them.
-Return the maximum score of a pair of sightseeing spots.
+// 1014. Best Sightseeing Pair
+// Medium
+// You are given an integer array values where values[i] represents the value of the ith sightseeing spot. 
+// Two sightseeing spots i and j have a distance j - i between them.
+// The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: 
+// the sum of the values of the sightseeing spots, minus the distance between them.
+// Return the maximum score of a pair of sightseeing spots.
 
-Example 1:
-Input: values = [8,1,5,2,6]
-Output: 11
-Explanation: i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
+// Example 1:
+// Input: values = [8,1,5,2,6]
+// Output: 11
+// Explanation: i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
 
-Example 2:
-Input: values = [1,2]
-Output: 2
+// Example 2:
+// Input: values = [1,2]
+// Output: 2
 
-@param {number[]} values
-@return {number}
+// @param {number[]} values
+// @return {number}
 
 var maxScoreSightseeingPair = function(values) {
     
