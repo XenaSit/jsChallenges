b6189ef0e7fa6d65797f0c81c061a32bcf04e1cc
XenaSit <sitnikovaxena@gmail.com>
2025-01-09 05:50:32 -0500

    LEET, branch:leet, the DAILY challenge: 2185. Counting Words With a Given Prefix

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -18146,25 +18146,25 @@ var countPrefixSuffixPairs = function(words) {
 
 console.log("==========================================")
 
-2185. Counting Words With a Given Prefix
-Easy
-You are given an array of strings words and a string pref.
-Return the number of strings in words that contain pref as a prefix.
-A prefix of a string s is any leading contiguous substring of s.
+// 2185. Counting Words With a Given Prefix
+// Easy
+// You are given an array of strings words and a string pref.
+// Return the number of strings in words that contain pref as a prefix.
+// A prefix of a string s is any leading contiguous substring of s.
 
-Example 1:
-Input: words = ["pay","attention","practice","attend"], pref = "at"
-Output: 2
-Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+// Example 1:
+// Input: words = ["pay","attention","practice","attend"], pref = "at"
+// Output: 2
+// Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
 
-Example 2:
-Input: words = ["leetcode","win","loops","success"], pref = "code"
-Output: 0
-Explanation: There are no strings that contain "code" as a prefix.
+// Example 2:
+// Input: words = ["leetcode","win","loops","success"], pref = "code"
+// Output: 0
+// Explanation: There are no strings that contain "code" as a prefix.
 
-@param {string[]} words
-@param {string} pref
-@return {number}
+// @param {string[]} words
+// @param {string} pref
+// @return {number}
 
 var prefixCount = function(words, pref) {
     
