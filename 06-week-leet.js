ce3d4139f80a897590e8b43409b136c0b95fd43f
XenaSit <sitnikovaxena@gmail.com>
2025-02-03 10:20:51 -0500

    LEET, branch:leet, the DAILY challenge: 3105. Longest Strictly Increasing or Strictly Decreasing Subarray

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -19755,8 +19755,30 @@ console.log("==========================================")
 // @return {number}
 
 var longestMonotonicSubarray = function(nums) {
+    if (nums.length === 0) return 0;
     
+    let maxLen = 1;
+    let incLen = 1;
+    let decLen = 1;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            incLen += 1;
+            decLen = 1;
+        } else if (nums[i] < nums[i - 1]) {
+            decLen += 1;
+            incLen = 1;
+        } else {
+            incLen = 1;
+            decLen = 1;
+        }
+        
+        maxLen = Math.max(maxLen, incLen, decLen);
+    }
+    
+    return maxLen;
 };
+
 
 console.log("==========================================")
 // console.log("==========================================")
