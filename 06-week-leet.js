614d4916db93d61bd80a498a4da22a0063f45f51
XenaSit <sitnikovaxena@gmail.com>
2025-02-05 10:16:46 -0500

    LEET, branch:leet, the DAILY challenge: 1790. Check if One String Swap Can Make Strings Equal

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -19827,30 +19827,30 @@ var maxAscendingSum = function(nums) {
 
 console.log("==========================================")
 
-1790. Check if One String Swap Can Make Strings Equal
-Easy
-You are given two strings s1 and s2 of equal length. 
-A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
-Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+// 1790. Check if One String Swap Can Make Strings Equal
+// Easy
+// You are given two strings s1 and s2 of equal length. 
+// A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+// Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
 
-Example 1:
-Input: s1 = "bank", s2 = "kanb"
-Output: true
-Explanation: For example, swap the first character with the last character of s2 to make "bank".
+// Example 1:
+// Input: s1 = "bank", s2 = "kanb"
+// Output: true
+// Explanation: For example, swap the first character with the last character of s2 to make "bank".
 
-Example 2:
-Input: s1 = "attack", s2 = "defend"
-Output: false
-Explanation: It is impossible to make them equal with one string swap.
+// Example 2:
+// Input: s1 = "attack", s2 = "defend"
+// Output: false
+// Explanation: It is impossible to make them equal with one string swap.
 
-Example 3:
-Input: s1 = "kelb", s2 = "kelb"
-Output: true
-Explanation: The two strings are already equal, so no string swap operation is required.
+// Example 3:
+// Input: s1 = "kelb", s2 = "kelb"
+// Output: true
+// Explanation: The two strings are already equal, so no string swap operation is required.
 
-@param {string} s1
-@param {string} s2
-@return {boolean}
+// @param {string} s1
+// @param {string} s2
+// @return {boolean}
 
 var areAlmostEqual = function(s1, s2) {
     
