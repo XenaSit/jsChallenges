3718572660ff71aef997d2a966ada2c23edb49fc
XenaSit <sitnikovaxena@gmail.com>
2025-02-10 08:47:44 -0500

    LEET, branch:leet, the DAILY challenge: 3174. Clear Digits

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -20190,28 +20190,28 @@ var countBadPairs = function(nums) {
 
 console.log("==========================================")
 
-3174. Clear Digits
-Easy
-You are given a string s.
-Your task is to remove all digits by doing this operation repeatedly:
-Delete the first digit and the closest non-digit character to its left.
-Return the resulting string after removing all digits.
+// 3174. Clear Digits
+// Easy
+// You are given a string s.
+// Your task is to remove all digits by doing this operation repeatedly:
+// Delete the first digit and the closest non-digit character to its left.
+// Return the resulting string after removing all digits.
 
-Example 1:
-Input: s = "abc"
-Output: "abc"
-Explanation:
-There is no digit in the string.
+// Example 1:
+// Input: s = "abc"
+// Output: "abc"
+// Explanation:
+// There is no digit in the string.
 
-Example 2:
-Input: s = "cb34"
-Output: ""
-Explanation:
-First, we apply the operation on s[2], and s becomes "c4".
-Then we apply the operation on s[1], and s becomes "".
+// Example 2:
+// Input: s = "cb34"
+// Output: ""
+// Explanation:
+// First, we apply the operation on s[2], and s becomes "c4".
+// Then we apply the operation on s[1], and s becomes "".
 
-@param {string} s
-@return {string}
+// @param {string} s
+// @return {string}
 
 var clearDigits = function(s) {
     
