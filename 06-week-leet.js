82cdc5603b47fa7a529db28cadee1f0e5b409127
XenaSit <sitnikovaxena@gmail.com>
2025-02-21 07:33:36 -0500

    LEET, branch:leet, the DAILY challenge: 1261. Find Elements in a Contaminated Binary Tree

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -20809,65 +20809,63 @@ var findDifferentBinaryString = function(nums) {
 };
 console.log("==========================================")
 
-1261. Find Elements in a Contaminated Binary Tree
-Medium
-Given a binary tree with the following rules:
-root.val == 0
-For any treeNode:
-If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
-If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
-Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
-Implement the FindElements class:
-FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
-bool find(int target) Returns true if the target value exists in the recovered binary tree.
+// 1261. Find Elements in a Contaminated Binary Tree
+// Medium
+// Given a binary tree with the following rules:
+// root.val == 0
+// For any treeNode:
+// If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+// If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+// Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+// Implement the FindElements class:
+// FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
+// bool find(int target) Returns true if the target value exists in the recovered binary tree.
 
-Example 1:
-Input
-["FindElements","find","find"]
-[[[-1,null,-1]],[1],[2]]
-Output
-[null,false,true]
-Explanation
-FindElements findElements = new FindElements([-1,null,-1]); 
-findElements.find(1); // return False 
-findElements.find(2); // return True 
+// Example 1:
+// Input
+// ["FindElements","find","find"]
+// [[[-1,null,-1]],[1],[2]]
+// Output
+// [null,false,true]
+// Explanation
+// FindElements findElements = new FindElements([-1,null,-1]); 
+// findElements.find(1); // return False 
+// findElements.find(2); // return True 
 
-Example 2:
-Input
-["FindElements","find","find","find"]
-[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
-Output
-[null,true,true,false]
-Explanation
-FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
-findElements.find(1); // return True
-findElements.find(3); // return True
-findElements.find(5); // return False
+// Example 2:
+// Input
+// ["FindElements","find","find","find"]
+// [[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+// Output
+// [null,true,true,false]
+// Explanation
+// FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+// findElements.find(1); // return True
+// findElements.find(3); // return True
+// findElements.find(5); // return False
 
-Example 3:
-Input
-["FindElements","find","find","find","find"]
-[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
-Output
-[null,true,false,false,true]
-Explanation
-FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
-findElements.find(2); // return True
-findElements.find(3); // return False
-findElements.find(4); // return False
-findElements.find(5); // return True
+// Example 3:
+// Input
+// ["FindElements","find","find","find","find"]
+// [[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+// Output
+// [null,true,false,false,true]
+// Explanation
+// FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+// findElements.find(2); // return True
+// findElements.find(3); // return False
+// findElements.find(4); // return False
+// findElements.find(5); // return True
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- */
+// Definition for a binary tree node.
+// function TreeNode(val, left, right) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+// }
+
+// @param {TreeNode} root
+
 class FindElements {
     constructor(root) {
         this.values = new Set();
@@ -20890,11 +20888,10 @@ class FindElements {
 }
 
 
-/** 
- * Your FindElements object will be instantiated and called as such:
- * var obj = new FindElements(root)
- * var param_1 = obj.find(target)
- */
+// Your FindElements object will be instantiated and called as such:
+// var obj = new FindElements(root)
+// var param_1 = obj.find(target)
+
 
 console.log("==========================================")
 // console.log("==========================================")
