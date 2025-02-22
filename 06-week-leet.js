54a9ca675282e26f7c778dfd609078ee72b7a6aa
XenaSit <sitnikovaxena@gmail.com>
2025-02-22 13:25:38 -0500

    LEET, branch:leet, the DAILY challenge: 1028. Recover a Tree From Preorder Traversal

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -20895,35 +20895,35 @@ class FindElements {
 
 console.log("==========================================")
 
-1028. Recover a Tree From Preorder Traversal
-Hard
-We run a preorder depth-first search (DFS) on the root of a binary tree.
-At each node in this traversal, we output D dashes (where D is the depth of this node), 
-then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.  The depth of the root node is 0.
-If a node has only one child, that child is guaranteed to be the left child.
-Given the output traversal of this traversal, recover the tree and return its root.
+// 1028. Recover a Tree From Preorder Traversal
+// Hard
+// We run a preorder depth-first search (DFS) on the root of a binary tree.
+// At each node in this traversal, we output D dashes (where D is the depth of this node), 
+// then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.  The depth of the root node is 0.
+// If a node has only one child, that child is guaranteed to be the left child.
+// Given the output traversal of this traversal, recover the tree and return its root.
 
-Example 1:
-Input: traversal = "1-2--3--4-5--6--7"
-Output: [1,2,5,3,4,6,7]
+// Example 1:
+// Input: traversal = "1-2--3--4-5--6--7"
+// Output: [1,2,5,3,4,6,7]
 
-Example 2:
-Input: traversal = "1-2--3---4-5--6---7"
-Output: [1,2,5,3,null,6,null,4,null,7]
+// Example 2:
+// Input: traversal = "1-2--3---4-5--6---7"
+// Output: [1,2,5,3,null,6,null,4,null,7]
 
-Example 3:
-Input: traversal = "1-401--349---90--88"
-Output: [1,401,null,349,88,90]
+// Example 3:
+// Input: traversal = "1-401--349---90--88"
+// Output: [1,401,null,349,88,90]
 
-Definition for a binary tree node.
-function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
-}
+// Definition for a binary tree node.
+// function TreeNode(val, left, right) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.left = (left===undefined ? null : left)
+//     this.right = (right===undefined ? null : right)
+// }
 
-@param {string} traversal
-@return {TreeNode}
+// @param {string} traversal
+// @return {TreeNode}
 
 var recoverFromPreorder = function(traversal) {
     
