74071ac342f2b8e275b0bfad301e7519fb709d99
XenaSit <sitnikovaxena@gmail.com>
2025-03-01 09:41:19 -0500

    LEET, branch:leet, the DAILY challenge: 2460. Apply Operations to an Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -21344,8 +21344,31 @@ console.log("==========================================")
 // @return {number[]}
 
 var applyOperations = function(nums) {
+    let n = nums.length;
     
+    // Step 1: Apply the operations
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+    
+    // Step 2: Move all zeros to the end
+    let result = [];
+    for (let num of nums) {
+        if (num !== 0) {
+            result.push(num);
+        }
+    }
+    
+    while (result.length < n) {
+        result.push(0);
+    }
+    
+    return result;
 };
+
 
 console.log("==========================================")
 // console.log("==========================================")
